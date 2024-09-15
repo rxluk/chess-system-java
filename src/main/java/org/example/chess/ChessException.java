64a896eb0b2f265219ec270fd27d37bb5da8faab
Lucas G. Costa <lucasg.exe@gmail.com>
2024-09-15 20:31:41 -0300

    ChessException and ChessPosition

---
+++ src/main/java/org/example/chess/ChessException.java
@@ -0,0 +1,8 @@
+package org.example.chess;
+
+public class ChessException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public ChessException(String msg) {
+        super(msg);
+    }
+}
