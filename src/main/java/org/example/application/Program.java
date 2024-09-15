3a935d897f4f95f675d24dbeef0d17c0de44d196
Lucas G. Costa <lucasg.exe@gmail.com>
2024-09-15 16:28:36 -0300

    First class: Position

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -1,9 +1,12 @@
 package org.example.application;
 
+import org.example.boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
