776dd9838961b02d9bebdd600a1ce0289fab2a4f
Lucas G. Costa <lucasg.exe@gmail.com>
2024-09-15 16:52:27 -0300

    Starting to implement Board and Piece

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -1,12 +1,12 @@
 package org.example.application;
 
+import org.example.boardgame.Board;
 import org.example.boardgame.Position;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
