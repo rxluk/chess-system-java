4f170e1af2e2e954ee99a07fb58bffbd76017cbe
Lucas G. Costa <lucasg.exe@gmail.com>
2024-09-17 20:45:42 -0300

    Moving Pieces

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -1,12 +1,29 @@
 package org.example.application;
 
 import org.example.chess.ChessMatch;
+import org.example.chess.ChessPiece;
+import org.example.chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.println("Source: ");
+            ChessPosition source =  UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.println("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturePiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
--- src/main/java/org/example/chess/ChessException.java
+++ src/main/java/org/example/chess/ChessException.java
@@ -1,7 +1,10 @@
 package org.example.chess;
 
-public class ChessException extends RuntimeException {
+import org.example.boardgame.BoardException;
+
+public class ChessException extends BoardException {
     private static final long serialVersionUID = 1L;
+
     public ChessException(String msg) {
         super(msg);
     }
